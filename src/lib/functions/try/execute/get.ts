21ec407f11517f48c5ea86fc317e7ad80ef22182
François Egner <mail@francois-egner.de>
2025-06-16 12:34:25 +0200

    Execution function will not run the whole chain and use a cached result instead

--- src/lib/functions/try/execute/get.ts
+++ src/lib/functions/try/execute/get.ts
@@ -2,8 +2,17 @@ import {Try} from "../../../Try";
 import {runSteps} from "../helpers";
 
 export async function get(tryObject: Try<unknown>){
+    if(tryObject.$internal.isComputed){
+        if(tryObject.$internal.finalResult?.isError())
+            throw tryObject.$internal.finalResult.getError();
+
+        return tryObject.$internal.finalResult!.getValue();
+    }
+
     const finalResult = await runSteps(tryObject.$internal.steps);
     tryObject.$internal.finalResult = finalResult;
+    tryObject.$internal.isComputed = true;
+
     if(finalResult.isError())
         throw finalResult.getError();
 
